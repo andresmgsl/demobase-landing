68bb41d1b7b494d8302ecaaab3360682e6faa632
Andres Guerrero <andresmgsl@gmail.com>
2021-09-12 22:15:36 +0200

    feat(email): more tests

--- js/submission-created.js
+++ js/submission-created.js
@@ -1,29 +1,24 @@
-import * as dotenv from 'dotenv';
-import * as fetch from 'node-fetch';
+require('dotenv').config();
+const fetch = require('node-fetch')
 
 const { BUTTONDOWN_TOKEN } = process.env;
-dotenv.config();
 
 exports.handler = async (event) => {
   const email = JSON.parse(event.body).payload.email;
 
   console.log(`Recieved a submission: ${email}`);
 
-  try {
-    await fetch("https://api.buttondown.email/v1/subscribers", {
-      method: "POST",
-      headers: {
-        Authorization: `Token ${BUTTONDOWN_TOKEN}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email }),
+  return fetch("https://api.buttondown.email/v1/subscribers", {
+    method: "POST",
+    headers: {
+      Authorization: `Token ${BUTTONDOWN_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(`Submitted to Buttondown:\n ${data}`);
     })
-
-    console.log(`Submitted to Buttondown:\n ${data}`);
-
-  } catch (error) {
-    console.log(`erro es -> ${error}`);
-    return ({ statusCode: 422, body: String(error) })
-  }
-
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
