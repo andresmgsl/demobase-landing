145240127f4b135e1cfa29107fa95110dda2fdd0
Andres Guerrero <andresmgsl@gmail.com>
2021-09-12 22:18:53 +0200

    feat(email): another test

--- js/submission-created.js
+++ js/submission-created.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const fetch = require('node-fetch')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const { BUTTONDOWN_TOKEN } = process.env;
 
