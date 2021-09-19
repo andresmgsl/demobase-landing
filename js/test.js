68fb1246f825a344da16fd2789934a5a08621070
Andres Guerrero <andresmgsl@gmail.com>
2021-09-19 20:45:58 +0200

    feat(email): maybe it works

--- js/test.js
+++ js/test.js
@@ -1,5 +1,6 @@
 exports.handler = async (event) => {
-  const email = JSON.parse(event.body).payload.email;
+  console.log(event);
+  //const email = JSON.parse(event.body).payload.email;
 
   console.log(`Recieved a submission: ${email}`);
 };
