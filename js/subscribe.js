aac5ed8e418eb6326965807c75f03bc485c42bd4
Andres Guerrero <andresmgsl@gmail.com>
2021-09-19 22:52:29 +0200

    feat(netlify): changed subscribe fn (promise to async/await)

--- js/subscribe.js
+++ js/subscribe.js
@@ -9,23 +9,36 @@ exports.handler = async (event) => {
 
   console.log(`Recieved a submission: ${email}`);
 
-  return fetch("https://api.buttondown.email/v1/subscribers", {
-    method: "POST",
-    headers: {
-      Authorization: `Token ${BUTTONDOWN_TOKEN}`,
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-    },
-    body: JSON.stringify({ email }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(`Submitted to Buttondown:\n ${data}`);
-    })
-    .catch((error) => {
-      console.log(`el error es: ${error}`)
-      return { statusCode: 422, body: String(error) };
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+  };
+
+  try {
+    const response = await fetch("https://api.buttondown.email/v1/subscribers", {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${BUTTONDOWN_TOKEN}`,
+        "Content-Type": "application/json",
+        ...headers
+      },
+      body: JSON.stringify({ email }),
     });
+    const data = response.json();
+    console.log("data: ", data);
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({data})
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({error: "Something went wrong!"})
+    }
+  }
 };
