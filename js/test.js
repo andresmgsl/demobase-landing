aa75065e10f7cb38aac3f1cfc381c5afafff0574
Andres Guerrero <andresmgsl@gmail.com>
2021-09-19 21:04:35 +0200

    feat(js): fucking netlify

--- js/test.js
+++ js/test.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-  console.log(event);
+  console.log(event, event.body);
   //const email = JSON.parse(event.body).payload.email;
 
   console.log(`Recieved a submission: ${email}`);
